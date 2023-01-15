0953bb675ff1c3d2b8991f530c6cd92a79c40bf8
Tasnim <tasnim.talaat23@gmail.com>
2023-01-15 19:37:34 +0200

    tas

---
+++ template-css/assets/js/index.js
@@ -0,0 +1,18 @@
+
+// window.addEventListener('scroll' , function(){
+//     console.log(window.scrollY);
+//     var windowScroll = window.scrollY}
+//     )
+var container = document.querySelector('.aboutme')
+    $('.icon-up').click(function(){
+        // console.log(window.scrollY);
+        // window.scrollY=0
+        // console.log(window.scrollY);
+        // console.log(container.offsetTop);
+        window.scrollTo(0,0)
+
+           })
+$(document).ready(function(){
+$('.welcome h1').show(1000,function(){
+        $('.para').fadeIn(4000)
+})})    
