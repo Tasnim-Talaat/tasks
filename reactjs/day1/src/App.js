93b974a25be48d5cf79747068f237324d3fc75cf
Tasnim <tasnim.talaat23@gmail.com>
2023-01-31 13:02:40 +0200

    day1

---
+++ reactjs/day1/src/App.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react'
+import Parent from './components/Parent/Parent'
+
+export default class App extends Component {
+  render() {
+    return <>
+    <Parent/>
+    </>
+  
+  }
+}
