8098b3e36f5ca17200c7084b950ef7df13e17646
Tasnim <tasnim.talaat23@gmail.com>
2023-01-31 13:00:36 +0200

    day1

--- reactjs/day1/src/components/About/About.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-export default class About extends Component {
-  render() {
-    console.log(this.props);
-    return <h1>{this.props.sayHello}</h1>;
-  }
-}
